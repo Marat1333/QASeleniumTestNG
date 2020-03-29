984eae617596fa9c5a7d3f2e0480f8bf892a5564
60080527 <ksolkin@luxoft.com>
2020-03-29 17:39:30 +0300

    Update Customers tests

--- src/test/java/com/leroy/magmobile/api/tests/cusomers/CustomerTest.java
+++ src/test/java/com/leroy/magmobile/api/tests/cusomers/CustomerTest.java
@@ -1,5 +1,6 @@
 package com.leroy.magmobile.api.tests.cusomers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.inject.Inject;
 import com.leroy.magmobile.api.clients.CustomerClient;
 import com.leroy.magmobile.api.data.customer.CustomerData;
@@ -40,6 +41,24 @@ public class CustomerTest extends BaseProjectApiTest {
             throw new IllegalArgumentException("customerData hasn't been created");
         Response<CustomerResponseBodyData> resp = customerClient.getCustomer(customerData.getCustomerNumber());
         customerClient.assertThatGetResponseMatches(resp, customerData);
+    }
+
+    @Test(description = "Update Customer - change gender")
+    public void testUpdateCustomer() {
+        if (customerData == null)
+            throw new IllegalArgumentException("customerData hasn't been created");
+        customerData.setGender(customerData.getGender().equals("male") ? "female" : "male");
+        Response<CustomerResponseBodyData> resp = customerClient.updateCustomer(customerData);
+        customerClient.assertThatIsCreatedAndGetData(resp, customerData);
+    }
+
+    // Negative scenarios
+
+    @Test(description = "Get Customer Balance  for non-existent account")
+    public void testGetCustomerBalanceForNonExistentAccount() {
+        String nonExistentAccountNumber = "999999";
+        Response<JsonNode> resp = customerClient.getCustomerBalance(nonExistentAccountNumber);
+        customerClient.assertThatBalanceNotFound(resp);
     }
 
 }
