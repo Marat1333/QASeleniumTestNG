b616196e8011f070a49bb982f6a8c7503d6c23cd
60080527 <ksolkin@luxoft.com>
2020-04-12 14:14:40 +0300

    clean up BaseSupplyPlanRequest class

--- src/test/java/com/leroy/magmobile/api/requests/supply_plan/BaseSupplyPlanRequest.java
+++ src/test/java/com/leroy/magmobile/api/requests/supply_plan/BaseSupplyPlanRequest.java
@@ -4,29 +4,15 @@ import com.leroy.magmobile.api.requests.CommonLegoRequest;
 
 import java.time.LocalDate;
 
-public class BaseSupplyPlanRequest<J extends BaseSupplyPlanRequest> extends CommonLegoRequest<BaseSupplyPlanRequest<J>> {
-    public J setDate(LocalDate... dates) {
+public class BaseSupplyPlanRequest<J extends BaseSupplyPlanRequest<J>> extends CommonLegoRequest<J> {
 
+    public J setDate(LocalDate... dates) {
         String[] strDates = new String[dates.length];
-        for (int i=0;i<dates.length;i++) {
-            strDates[i]=dates[i].toString();
+        for (int i = 0; i < dates.length; i++) {
+            strDates[i] = dates[i].toString();
         }
         String result = String.join(",", strDates);
-        return (J) queryParam("dates", result);
+        return queryParam("dates", result);
     }
 
-    @Override
-    public J setLdap(String val) {
-        return (J) super.setLdap(val);
-    }
-
-    @Override
-    public J setShopId(String val) {
-        return (J) super.setShopId(val);
-    }
-
-    @Override
-    public J setDepartmentId(Object val) {
-        return (J) super.setDepartmentId(val);
-    }
 }
