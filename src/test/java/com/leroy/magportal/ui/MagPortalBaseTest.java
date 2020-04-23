b4f8460709d7875632605189a805de9faff97629
60080527 <ksolkin@luxoft.com>
2020-04-23 10:20:53 +0300

    add init sessionData in MagPortalBaseTest

--- src/test/java/com/leroy/magportal/ui/MagPortalBaseTest.java
+++ src/test/java/com/leroy/magportal/ui/MagPortalBaseTest.java
@@ -2,10 +2,12 @@ package com.leroy.magportal.ui;
 
 import com.leroy.constants.EnvConstants;
 import com.leroy.core.BaseUiTest;
+import com.leroy.core.SessionData;
 import com.leroy.core.TestContext;
 import com.leroy.core.asserts.CustomAssert;
 import com.leroy.core.asserts.CustomSoftAssert;
 import com.leroy.core.testrail.helpers.StepLog;
+import com.leroy.magmobile.ui.Context;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,7 +15,8 @@ import java.lang.reflect.Method;
 
 public class MagPortalBaseTest extends BaseUiTest {
 
-    TestContext context;
+    Context context;
+    protected SessionData sessionData;
 
     @Override
     protected void cleanContext() {
@@ -22,7 +25,10 @@ public class MagPortalBaseTest extends BaseUiTest {
 
     @Override
     protected void initContext(WebDriver driver) {
-        context = new TestContext(driver);
+        context = new Context(driver);
+        sessionData = new SessionData();
+        sessionData.setUserLdap(EnvConstants.BASIC_USER_LDAP);
+        context.setSessionData(sessionData);
     }
 
     @Override
