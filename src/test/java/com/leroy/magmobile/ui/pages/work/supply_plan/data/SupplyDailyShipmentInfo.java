e6d86191433d71783decdb33786a2fda4b3a7033
G1nes <timursalakhov96@gmail.com>
2020-07-16 16:05:21 +0300

    complete C3293192

--- src/test/java/com/leroy/magmobile/ui/pages/work/supply_plan/data/SupplyDailyShipmentInfo.java
+++ src/test/java/com/leroy/magmobile/ui/pages/work/supply_plan/data/SupplyDailyShipmentInfo.java
@@ -3,12 +3,14 @@ package com.leroy.magmobile.ui.pages.work.supply_plan.data;
 import com.leroy.magmobile.api.data.supply_plan.Details.ShipmentData;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class SupplyDailyShipmentInfo {
     String departmentId;
     ShipmentData data;
 
-    public SupplyDailyShipmentInfo(ShipmentData data, String departmentId) {
+    public SupplyDailyShipmentInfo(ShipmentData data, String departmentId){
         this.data = data;
         this.departmentId = departmentId;
     }
