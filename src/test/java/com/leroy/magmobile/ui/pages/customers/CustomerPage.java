6f6d202bf7e96a9979b389db88643ac452a2d862
60080527 <ksolkin@luxoft.com>
2020-08-26 00:01:28 +0300

    C3201018 add

--- src/test/java/com/leroy/magmobile/ui/pages/customers/CustomerPage.java
+++ src/test/java/com/leroy/magmobile/ui/pages/customers/CustomerPage.java
@@ -1,14 +1,27 @@
 package com.leroy.magmobile.ui.pages.customers;
 
 import com.leroy.core.annotations.AppFindBy;
+import com.leroy.core.fieldfactory.CustomLocator;
+import com.leroy.core.web_elements.android.AndroidScrollView;
+import com.leroy.core.web_elements.general.Element;
 import com.leroy.magmobile.ui.elements.MagMobButton;
+import com.leroy.magmobile.ui.models.customer.MagCustomerData;
 import com.leroy.magmobile.ui.pages.common.TopMenuPage;
+import com.leroy.magmobile.ui.pages.common.widget.CardWidget;
+import com.leroy.utils.ParserUtil;
 import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
 
 // Раздел "Продажа" -> Страница "Клиенты"
 public class CustomerPage extends TopMenuPage {
 
-    @AppFindBy(text = "СОЗДАТЬ НОВВОГО КЛИЕНТА")
+    AndroidScrollView<MagCustomerData> recentCustomerScrollView = new AndroidScrollView<>(
+            driver, AndroidScrollView.TYPICAL_LOCATOR,
+            "//android.view.ViewGroup[android.view.ViewGroup[android.widget.TextView[@text='НЕДАВНИЕ КЛИЕНТЫ']]]/following-sibling::android.view.ViewGroup[android.view.ViewGroup]",
+            CustomerWidget.class
+    );
+
+    @AppFindBy(text = "СОЗДАТЬ НОВОГО КЛИЕНТА")
     MagMobButton createNewCustomerBtn;
 
     // Actions
@@ -17,6 +30,43 @@ public class CustomerPage extends TopMenuPage {
     public NewCustomerInfoPage clickCreateNewCustomer() throws Exception {
         createNewCustomerBtn.click();
         return new NewCustomerInfoPage();
+    }
+
+    // Verifications
+
+    @Step("Проверить, что {index} клиент соответствуют ожидаемым данным")
+    public CustomerPage shouldRecentCustomerIs(int index, MagCustomerData expectedData) {
+        index--;
+        recentCustomerScrollView.getDataObj(index).assertEqualsNotNullExpectedFields(expectedData);
+        return this;
+    }
+
+    // Widget
+
+    private static class CustomerWidget extends CardWidget<MagCustomerData> {
+
+        public CustomerWidget(WebDriver driver, CustomLocator locator) {
+            super(driver, locator);
+        }
+
+        @AppFindBy(xpath = ".//android.widget.TextView[1]", metaName = "Имя клиента")
+        Element name;
+
+        @AppFindBy(xpath = ".//android.widget.TextView[2]", metaName = "Телефон клиента")
+        Element phone;
+
+        @Override
+        public MagCustomerData collectDataFromPage(String pageSource) {
+            MagCustomerData magCustomerData = new MagCustomerData();
+            magCustomerData.setName(name.getText(pageSource));
+            magCustomerData.setPhone(ParserUtil.standardPhoneFmt(phone.getText(pageSource)));
+            return magCustomerData;
+        }
+
+        @Override
+        public boolean isFullyVisible(String pageSource) {
+            return phone.isVisible(pageSource) && name.isVisible(pageSource);
+        }
     }
 
 }
--- src/test/java/com/leroy/magmobile/ui/tests/sales/CustomerTest.java
+++ src/test/java/com/leroy/magmobile/ui/tests/sales/CustomerTest.java
@@ -1,9 +1,15 @@
 package com.leroy.magmobile.ui.tests.sales;
 
+import com.leroy.constants.Gender;
 import com.leroy.magmobile.ui.AppBaseSteps;
+import com.leroy.magmobile.ui.models.customer.MagCustomerData;
 import com.leroy.magmobile.ui.pages.customers.CustomerPage;
 import com.leroy.magmobile.ui.pages.customers.NewCustomerInfoPage;
+import com.leroy.magmobile.ui.pages.customers.SuccessCustomerPage;
+import com.leroy.utils.RandomUtil;
 import org.testng.annotations.Test;
+
+import java.util.Random;
 
 public class CustomerTest extends AppBaseSteps {
 
@@ -13,32 +19,46 @@ public class CustomerTest extends AppBaseSteps {
 
         // Step 1
         step("Нажмите на кнопку Создать нового клиента");
-        NewCustomerInfoPage newCustomerInfoPage = customerPage.clickCreateNewCustomer();
+        NewCustomerInfoPage newCustomerInfoPage = customerPage.clickCreateNewCustomer()
+                .verifyRequiredElements(false);
 
         // Steps 2-4
         step("Введите имя нового клиента");
+        String customerFirstName = RandomUtil.randomCyrillicCharacters(7);
+        newCustomerInfoPage.editCustomerFirstName(customerFirstName, true);
 
         // Step 5
         step("Выберите пол клиента");
+        Gender gender = new Random().nextBoolean() ? Gender.MALE : Gender.FEMALE;
+        newCustomerInfoPage.selectGender(gender, true);
 
-        // Steps 6-8
+        // Steps 6-9
         step("Ведите новый номер телефона");
-
-        // Step 9
-        step("Нажмите на поле галочки.");
+        String phone = RandomUtil.randomPhoneNumber();
+        newCustomerInfoPage.editPhoneNumber(phone, true);
 
         // Step 10
         step("Нажмите на Показать все поля");
+        newCustomerInfoPage.clickShowAllFieldsButton()
+                .verifyRequiredElements(true);
 
         // Step 11
         step("Нажмите на Скрыть дополнительные поля");
+        newCustomerInfoPage.clickHideAdditionalFieldsButton()
+                .verifyRequiredElements(false);
 
         // Step 12
         step("Нажмите на Создать");
+        SuccessCustomerPage successCustomerPage = newCustomerInfoPage.clickSubmitButton();
+        successCustomerPage.verifyRequiredElements();
 
         // Step 13
         step("Нажмите на Перейти к списку клиентов");
-
+        MagCustomerData customerData = new MagCustomerData();
+        customerData.setPhone(phone);
+        customerData.setName(customerFirstName);
+        customerPage = successCustomerPage.clickGoToCustomerListButton();
+        customerPage.shouldRecentCustomerIs(1, customerData);
     }
 
 }
