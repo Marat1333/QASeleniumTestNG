4a30dc17565f7b0b68c83f5240bf59a58b4b4147
G1nes <timursalakhov96@gmail.com>
2020-08-26 15:41:46 +0300

    fix comment

--- src/test/java/com/leroy/magmobile/ui/pages/work/ruptures/ActionsModalPage.java
+++ src/test/java/com/leroy/magmobile/ui/pages/work/ruptures/ActionsModalPage.java
@@ -70,7 +70,7 @@ public class ActionsModalPage extends CommonMagMobilePage {
     @Step("Выбрать задачу")
     public ActionsModalPage choseTasks(String... taskNames) {
         for (String taskName : taskNames) {
-            int arrayLength = taskNames.length;
+            int arrayLength = toDoTasksList.getCount();
             E(String.format("//*[@text='%s']/following-sibling::android.view.ViewGroup[@content-desc='Button-container'][1]",
                     taskName)).click();
             toDoTasksList.waitUntilSizeHasChanged(arrayLength);
