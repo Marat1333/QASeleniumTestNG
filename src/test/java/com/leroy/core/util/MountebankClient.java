3d2d6254974474927f21e9df8a409628a9ff4e1e
60080527 <ksolkin@luxoft.com>
2020-10-08 18:23:34 +0300

    Mock search test C3200996

---
+++ src/test/java/com/leroy/core/util/MountebankClient.java
@@ -0,0 +1,39 @@
+package com.leroy.core.util;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.mbtest.javabank.Client;
+
+public class MountebankClient extends Client {
+
+    public MountebankClient(String baseUrl) {
+        super(baseUrl);
+    }
+
+    public int addStubs(JSONObject jsonStubsBody, int port) {
+        try {
+            int status = 0;
+            JSONArray stubs = (JSONArray) jsonStubsBody.get("stubs");
+            for (Object obj : stubs) {
+                JSONObject oneStubBody = (JSONObject) obj;
+                JSONObject stub = new JSONObject();
+                stub.put("index", 0);
+                stub.put("stub", oneStubBody);
+
+                HttpResponse<JsonNode> response = Unirest.post(this.baseUrl + "/imposters/" + port + "/stubs")
+                        .body(stub.toJSONString()).asJson();
+                status = response.getStatus();
+                if (status != 200)
+                    return status;
+            }
+            return status;
+        } catch (UnirestException var3) {
+            return 500;
+        }
+    }
+
+}
