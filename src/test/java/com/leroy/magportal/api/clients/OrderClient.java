601c44c515b52be107792f6ebd46889b75f2379a
smoskal <smoskal@luxoft.com>
2020-11-12 10:23:42 +0300

    Code was formatted.

--- src/test/java/com/leroy/magportal/api/clients/OrderClient.java
+++ src/test/java/com/leroy/magportal/api/clients/OrderClient.java
@@ -660,11 +660,16 @@ public class OrderClient extends com.leroy.magmobile.api.clients.OrderClient {
         SoftAssert softAssert = new SoftAssert();
 //        PaymentTypeEnum.values().
         if (currentOrderType == null) {
-            softAssert.assertEquals(orderData.getChannel(), OrderChannelEnum.OFFLINE.getValue(), "Channel is invalid.");
-            softAssert.assertEquals(orderData.getPaymentType(), PaymentTypeEnum.CASH_OFFLINE.getMashName(), "Payment Type is invalid.");
+            softAssert.assertEquals(orderData.getChannel(), OrderChannelEnum.OFFLINE.getValue(),
+                    "Channel is invalid.");
+            softAssert.assertEquals(orderData.getPaymentType(),
+                    PaymentTypeEnum.CASH_OFFLINE.getMashName(), "Payment Type is invalid.");
         } else {
-            softAssert.assertEquals(orderData.getChannel(), OrderChannelEnum.ONLINE.getValue(), "Channel is invalid.");
-            softAssert.assertEquals(orderData.getPaymentType(), PaymentTypeEnum.getMashNameByName(currentOrderType.paymentType), "Payment Type is invalid.");
+            softAssert.assertEquals(orderData.getChannel(), OrderChannelEnum.ONLINE.getValue(),
+                    "Channel is invalid.");
+            softAssert.assertEquals(orderData.getPaymentType(),
+                    PaymentTypeEnum.getMashNameByName(currentOrderType.paymentType),
+                    "Payment Type is invalid.");
             if (!currentOrderType.getDeliveryServiceType()
                     .equals(DeliveryServiceTypeEnum.PICKUP.getService())) {
                 softAssert.assertTrue(orderData.getDelivery(), "Delivery is invalid.");
@@ -673,14 +678,20 @@ public class OrderClient extends com.leroy.magmobile.api.clients.OrderClient {
                 softAssert.assertFalse(orderData.getDelivery(), "Delivery is invalid.");
             }
         }
-        softAssert.assertTrue(orderData.getProducts().size() > 0,"There are No Products in order.");
-        softAssert.assertTrue(Strings.isNotNullAndNotEmpty(orderData.getFulfillmentTaskId()),"FulfillmentTaskId is empty.");
-        softAssert.assertTrue(orderData.getFulfillmentVersion() > 0,"FulfillmentVersion is empty.");
-        softAssert.assertTrue(Strings.isNotNullAndNotEmpty(orderData.getOrderId()),"OrderId is empty.");
-        softAssert.assertTrue(Strings.isNotNullAndNotEmpty(orderData.getPaymentTaskId()),"PaymentTaskId is empty.");
-        softAssert.assertTrue(Strings.isNotNullAndNotEmpty(orderData.getPaymentStatus()),"PaymentStatus is empty.");
-        softAssert.assertTrue(orderData.getPaymentVersion() > 0,"PaymentVersion is empty.");
-        softAssert.assertTrue(orderData.getSolutionVersion() > 0,"SolutionVersion is empty.");
+        softAssert
+                .assertTrue(orderData.getProducts().size() > 0, "There are No Products in order.");
+        softAssert.assertTrue(Strings.isNotNullAndNotEmpty(orderData.getFulfillmentTaskId()),
+                "FulfillmentTaskId is empty.");
+        softAssert
+                .assertTrue(orderData.getFulfillmentVersion() > 0, "FulfillmentVersion is empty.");
+        softAssert.assertTrue(Strings.isNotNullAndNotEmpty(orderData.getOrderId()),
+                "OrderId is empty.");
+        softAssert.assertTrue(Strings.isNotNullAndNotEmpty(orderData.getPaymentTaskId()),
+                "PaymentTaskId is empty.");
+        softAssert.assertTrue(Strings.isNotNullAndNotEmpty(orderData.getPaymentStatus()),
+                "PaymentStatus is empty.");
+        softAssert.assertTrue(orderData.getPaymentVersion() > 0, "PaymentVersion is empty.");
+        softAssert.assertTrue(orderData.getSolutionVersion() > 0, "SolutionVersion is empty.");
 
         softAssert.assertAll();
     }
