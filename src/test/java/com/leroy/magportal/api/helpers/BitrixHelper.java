c4196de1c3d83488d897a846dd971ced3c8ba94b
60080527 <ksolkin@luxoft.com>
2020-11-16 16:18:34 +0300

    moving customer mashup client, data, requests, etc

--- src/test/java/com/leroy/magportal/api/helpers/BitrixHelper.java
+++ src/test/java/com/leroy/magportal/api/helpers/BitrixHelper.java
@@ -5,11 +5,11 @@ import static com.leroy.magportal.ui.constants.TestDataConstants.SIMPLE_CUSTOMER
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.google.inject.Inject;
-import com.leroy.common_mashups.clients.CustomerClient;
-import com.leroy.common_mashups.data.customer.CustomerListData;
-import com.leroy.common_mashups.data.customer.CustomerSearchFilters;
-import com.leroy.common_mashups.data.customer.CustomerSearchFilters.CustomerType;
-import com.leroy.common_mashups.data.customer.CustomerSearchFilters.DiscriminantType;
+import com.leroy.common_mashups.customer_accounts.clients.CustomerClient;
+import com.leroy.common_mashups.customer_accounts.data.CustomerListData;
+import com.leroy.common_mashups.customer_accounts.data.CustomerSearchFilters;
+import com.leroy.common_mashups.customer_accounts.data.CustomerSearchFilters.CustomerType;
+import com.leroy.common_mashups.customer_accounts.data.CustomerSearchFilters.DiscriminantType;
 import com.leroy.common_mashups.helpers.SearchProductHelper;
 import com.leroy.constants.sales.SalesDocumentsConst.States;
 import com.leroy.core.api.ThreadApiClient;
