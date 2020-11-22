a8981cbbb28919c79f8005ad9102eea945dd708d
60080527 <ksolkin@luxoft.com>
2020-11-22 17:40:13 +0300

    Add C23415580

--- src/test/java/com/leroy/magportal/ui/tests/picking/PickingWavesMobileTest.java
+++ src/test/java/com/leroy/magportal/ui/tests/picking/PickingWavesMobileTest.java
@@ -1,30 +1,9 @@
 package com.leroy.magportal.ui.tests.picking;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.google.inject.Inject;
-import com.leroy.constants.sales.SalesDocumentsConst;
-import com.leroy.core.ContextProvider;
-import com.leroy.core.UserSessionData;
-import com.leroy.magmobile.api.data.sales.cart_estimate.cart.CartProductOrderData;
-import com.leroy.magmobile.api.data.sales.orders.GiveAwayData;
-import com.leroy.magportal.api.clients.OrderClient;
-import com.leroy.magportal.api.clients.PickingTaskClient;
-import com.leroy.magportal.api.data.picking.PickingTaskDataList;
-import com.leroy.magportal.api.helpers.PAOHelper;
 import com.leroy.magportal.ui.pages.picking.mobile.PickingDocListMobilePage;
+import com.leroy.magportal.ui.pages.picking.mobile.PickingWaveMobilePage;
 import com.leroy.magportal.ui.tests.BaseMockMagPortalUiTest;
-import com.leroy.magportal.ui.tests.BasePAOTest;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
-import ru.leroymerlin.qa.core.clients.base.Response;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.leroy.constants.sales.SalesDocumentsConst.States.PICKED;
-import static com.leroy.core.matchers.Matchers.successful;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PickingWavesMobileTest extends BaseMockMagPortalUiTest {
 
@@ -107,29 +86,49 @@ public class PickingWavesMobileTest extends BaseMockMagPortalUiTest {
         }
     }*/
 
-    @Test(description = "C23415580 Добавление сборок в волну"/*, groups = NEED_PRODUCTS_GROUP*/)
+    @Test(description = "C23415580 Добавление сборок в волну")
     public void testAddPickingIntoWave() throws Exception {
         setUpMockForTestCase();
-        /*initCreateOrder(1);
-        String order1 = orderId;
-        initFindPickingTask();
-        String assemblyNumber = pickingTaskId.substring(pickingTaskId.length() - 4);
-        String orderNumber = orderId.substring(orderId.length() - 4);
-        String fullNumber1 = assemblyNumber + " *" + orderNumber;
-        initCreateOrder(1);
-        initFindPickingTask();
-        assemblyNumber = pickingTaskId.substring(pickingTaskId.length() - 4);
-        orderNumber = orderId.substring(orderId.length() - 4);
-        String fullNumber2 = assemblyNumber + " *" + orderNumber;
-        String order2 = orderId;*/
 
         // Step 1
         step("Открыть страницу со Сборкой");
         PickingDocListMobilePage pickingPage = loginAndGoTo(PickingDocListMobilePage.class);
         pickingPage.reloadPage();
+        pickingPage = new PickingDocListMobilePage();
 
         // Step 2
         step("Нажать на иконку волны сборок");
+        PickingWaveMobilePage pickingWaveMobilePage = pickingPage.clickPickingWageButton();
+
+        // Step 3
+        step("Проверить список товаров, если они есть");
+        pickingWaveMobilePage.shouldTitleIsVisible();
+
+        // Step 4
+        step("Вернуться на экран Сборки");
+        pickingWaveMobilePage.clickBackButton();
+
+        // Step 5
+        step("Выбрать несколько сборок, нажав на чекбокс 'Выбрать для сборки'");
+        pickingPage = new PickingDocListMobilePage();
+        pickingPage.selectAllPickingChkBoxes()
+                .checkActiveButtonsVisibility(true);
+
+        // Step 6
+        step("Нажать кнопку 'Отмена'");
+        pickingPage.clickCancelButton()
+                .checkActiveButtonsVisibility(false);
+
+        // Step 7
+        step("Выбрать несколько сборок, нажав на чекбокс 'Выбрать для сборки', Нажать кнопку 'Начать сборку'");
+        pickingPage.selectAllPickingChkBoxes()
+                .clickStartPicking()
+                .checkActiveButtonsVisibility(false);
+
+        // Step 8
+        step("Нажать на иконку волны сборок");
+        pickingPage.clickPickingWageButton()
+                .shouldTitleIsVisible();
     }
 
 }
