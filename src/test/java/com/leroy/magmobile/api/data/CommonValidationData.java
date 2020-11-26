326802f3964350d59f0b25e44afc8ed7075f76db
60080527 <ksolkin@luxoft.com>
2020-11-26 18:19:05 +0300

    minor update

--- src/test/java/com/leroy/magmobile/api/data/CommonValidationData.java
+++ src/test/java/com/leroy/magmobile/api/data/CommonValidationData.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class CommonValidationData {
     private String shopid;
-    private String shopId;
     private String departmentId;
     private String sessionId;
     private String lmCode;
