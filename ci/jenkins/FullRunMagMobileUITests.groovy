7f637fb9641903ec75c0db7abe1a76f224b37467
60080527 <ksolkin@luxoft.com>
2020-12-04 02:56:03 +0300

    Add pipelines

--- ci/jenkins/FullRunMagMobileUITests.groovy
+++ ci/jenkins/FullRunMagMobileUITests.groovy
@@ -3,13 +3,13 @@ timestamps {
         try {
             stage('Run job #1') {
                 build job: 'lego-front-android-Run-UI_TESTS', parameters: [
-                        [$class: 'StringParameterValue', name: 'BRANCH', value: 'kas/update_build_apk'],
+                        [$class: 'StringParameterValue', name: 'BRANCH', value: 'dev'],
                         [$class: 'StringParameterValue', name: 'TEST_RUN', value: 'true'],
                         [$class: 'StringParameterValue', name: 'TELEGRAM_CHAT', value: env.TELEGRAM_CHAT],
                         [$class: 'StringParameterValue', name: 'TEST_THREAD_COUNT', value: env.TEST_THREAD_COUNT],
                         [$class: 'StringParameterValue', name: 'TEST_XML', value: 'UiGlobalMobileSuite.xml'],
                         [$class: 'StringParameterValue', name: 'RUN_NAME', value: env.RUN_NAME],
-                        [$class: 'StringParameterValue', name: 'RETRY_COUNT', value: '0'],
+                        [$class: 'StringParameterValue', name: 'RETRY_COUNT', value: '1'],
                         [$class: 'StringParameterValue', name: 'ONLY_SMOKE', value: 'false'],
                         [$class: 'StringParameterValue', name: 'BUILD_TYPE', value: 'TEST'],
                         [$class: 'StringParameterValue', name: 'CHECK_CHANGES', value: 'true'],
@@ -24,7 +24,7 @@ timestamps {
         } finally {
             stage('Run job #2 (Mock)') {
                 build job: 'lego-front-android-Run-UI_TESTS', parameters: [
-                        [$class: 'StringParameterValue', name: 'BRANCH', value: 'kas/update_build_apk'],
+                        [$class: 'StringParameterValue', name: 'BRANCH', value: 'dev'],
                         [$class: 'StringParameterValue', name: 'TEST_RUN', value: 'true'],
                         [$class: 'StringParameterValue', name: 'TELEGRAM_CHAT', value: env.TELEGRAM_CHAT],
                         [$class: 'StringParameterValue', name: 'TEST_THREAD_COUNT', value: env.TEST_THREAD_COUNT],
---
+++ ci/jenkins/FullRunMagPortalUITests.groovy
@@ -0,0 +1,46 @@
+timestamps {
+    node("dockerhost") {
+        try {
+            stage('Run job #1') {
+                build job: 'PUZ2-UI-autotests', parameters: [
+                        [$class: 'StringParameterValue', name: 'SUITE_XML', value: 'UiGlobalMockCasesSuite.xml'],
+                        [$class: 'StringParameterValue', name: 'ENVIROMENT', value: 'portal_mock'],
+                        [$class: 'StringParameterValue', name: 'THREAD_COUNT', value: env.THREAD_COUNT],
+                        [$class: 'StringParameterValue', name: 'RUN', value: env.RUN_NAME],
+                        [$class: 'BooleanParameterValue', name: 'RUN_CASE_WITH_ISSUE', value : true],
+                        [$class: 'StringParameterValue', name: 'TELEGRAM_CHAT', value: env.TELEGRAM_CHAT],
+                        [$class: 'StringParameterValue', name: 'RETRY_COUNT', value: '1'],
+                        [$class: 'StringParameterValue', name: 'TEST_CONFIG', value: 'chrome_grid']
+                ]
+            }
+        } finally {
+            try {
+                stage('Run job #2') {
+                    build job: 'PUZ2-UI-autotests', parameters: [
+                            [$class: 'StringParameterValue', name: 'SUITE_XML', value: 'UiMobileCasesSuite.xml'],
+                            [$class: 'StringParameterValue', name: 'ENVIROMENT', value: 'portal_mock'],
+                            [$class: 'StringParameterValue', name: 'THREAD_COUNT', value: env.THREAD_COUNT],
+                            [$class: 'StringParameterValue', name: 'RUN', value: env.RUN_NAME],
+                            [$class: 'BooleanParameterValue', name: 'RUN_CASE_WITH_ISSUE', value: true],
+                            [$class: 'StringParameterValue', name: 'TELEGRAM_CHAT', value: env.TELEGRAM_CHAT],
+                            [$class: 'StringParameterValue', name: 'RETRY_COUNT', value: '1'],
+                            [$class: 'StringParameterValue', name: 'TEST_CONFIG', value: 'android_web_grid']
+                    ]
+                }
+            } finally {
+                stage('Run job #3') {
+                    build job: 'PUZ2-UI-autotests', parameters: [
+                            [$class: 'StringParameterValue', name: 'SUITE_XML', value: 'UiGlobalPortalSuite.xml'],
+                            [$class: 'StringParameterValue', name: 'ENVIROMENT', value: env.ENVIRONMENT],
+                            [$class: 'StringParameterValue', name: 'THREAD_COUNT', value: env.THREAD_COUNT],
+                            [$class: 'StringParameterValue', name: 'RUN', value: env.RUN_NAME],
+                            [$class: 'BooleanParameterValue', name: 'RUN_CASE_WITH_ISSUE', value: true],
+                            [$class: 'StringParameterValue', name: 'TELEGRAM_CHAT', value: env.TELEGRAM_CHAT],
+                            [$class: 'StringParameterValue', name: 'RETRY_COUNT', value: '1'],
+                            [$class: 'StringParameterValue', name: 'TEST_CONFIG', value: 'chrome_grid']
+                    ]
+                }
+            }
+        }
+    }
+}
