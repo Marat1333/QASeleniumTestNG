8d44bfa21130c7d46041d3d14dd67d326cb80048
Kartsev Evgeniy <ekartsev@luxoft.com>
2021-02-11 19:44:55 +0300

    move enums to PickingConst

--- src/test/java/com/leroy/magportal/ui/constants/picking/PickingConst.java
+++ src/test/java/com/leroy/magportal/ui/constants/picking/PickingConst.java
@@ -6,4 +6,46 @@ public class PickingConst {
         SHOPPING_ROOM, STOCK, SS
     }
 
+    public enum PickingStatus {
+        READY_TO_PICKING,
+        PICKING,
+        PICKING_PAUSE,
+        PARTIALLY_PICKED,
+        PICKED;
+    }
+
+    public enum OrderType {
+        ONLINE,
+        OFFLINE;
+    }
+
+    public enum ClientType {
+        ENTITY,
+        CLIENT,
+        PROFI;
+    }
+
+    public enum SaleScheme {
+        LT,
+        LTD,
+        SLT,
+        SLTD,
+        SLTX;
+    }
+
+    public enum DeliveryType {
+        PICKUP,
+        DELIVERY_TK,
+        DELIVERY_PVZ,
+        DELIVERY_KK;
+    }
+
+    public enum Tag {
+        DEBT,
+        RISK_OF_NOT_COLLECT,
+        RISK_NOT_TO_SHIP,
+        PICKING_TODAY,
+        FOR_FUTURE_DATES;
+    }
+
 }
