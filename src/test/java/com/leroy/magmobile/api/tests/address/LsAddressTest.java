cb976a49704a43b28665467dc4910e6ec6a9a50f
Evgeny Alferyev <ealferyev@gmail.com>
2021-03-11 16:13:57 +0500

    Refactoring to create test data. Add a new method to other tests

--- src/test/java/com/leroy/magmobile/api/tests/address/LsAddressTest.java
+++ src/test/java/com/leroy/magmobile/api/tests/address/LsAddressTest.java
@@ -42,6 +42,9 @@ public class LsAddressTest extends BaseProjectApiTest {
     private LsAddressHelper lsAddressHelper;
 
     @Inject
+    private StandData standData;
+
+    @Inject
     private StandDataList standDataList;
 
     @Inject
@@ -56,6 +59,24 @@ public class LsAddressTest extends BaseProjectApiTest {
 
     private int createdAlleyId;
 
+    private void prepareDefaultData(boolean requiredStand, boolean requiredCell) {
+        step("Create a new alley");
+        alleyData = lsAddressHelper.createRandomAlley();
+        createdAlleyId = alleyData.getId();
+
+        if (requiredStand) {
+            step("Create new stands");
+            standDataList = lsAddressHelper.createDefaultStands(alleyData);
+
+            step("Get first stand from list");
+            standData = lsAddressHelper.getStandFromList(0, alleyData.getId());
+
+            if (requiredCell) {
+                step("Create new cells");
+                cellDataList = lsAddressHelper.createDefaultCells(standData.getId());
+            }
+        }
+    }
 
     @Override
     protected UserSessionData initTestClassUserSessionDataTemplate() {
@@ -93,10 +114,7 @@ public class LsAddressTest extends BaseProjectApiTest {
 
     @Test(description = "C23415877 lsAddress PUT alleys - rename alleys")
     public void testRenameAlleys() {
-        step("Create new alley");
-        alleyData = lsAddressHelper.createRandomAlley();
-        createdAlleyId = alleyData.getId();
-
+        prepareDefaultData(false, false);
         step("Rename alley");
         alleyData.setCode("24700");
         Response<AlleyData> renameResp = lsAddressClient.renameAlley(alleyData);
@@ -105,9 +123,7 @@ public class LsAddressTest extends BaseProjectApiTest {
 
     @Test(description = "C23415876 lsAddress DELETE alleys - delete alley")
     public void testDeleteAlleys() {
-        step("Create new alley");
-        alleyData = lsAddressHelper.createRandomAlley();
-        createdAlleyId = alleyData.getId();
+        prepareDefaultData(false, false);
 
         step("Delete alley");
         Response<AlleyData> deleteResp = lsAddressClient.deleteAlley(alleyData);
@@ -152,12 +168,7 @@ public class LsAddressTest extends BaseProjectApiTest {
 
     @Test(description = "C3316322 lsAddress POST cells")
     public void testCreateCell() {
-        step("Create a new alley");
-        AlleyData alleyData = lsAddressHelper.createRandomAlley();
-        createdAlleyId = alleyData.getId();
-
-        step("Create new stands");
-        standDataList = lsAddressHelper.createDefaultStands(alleyData);
+        prepareDefaultData(true, false);
 
         step("Create new cells");
         int standId = standDataList.getItems().get(0).getId();
@@ -166,23 +177,13 @@ public class LsAddressTest extends BaseProjectApiTest {
         cellDataList.setItems(Arrays.asList(itemData1, itemData2));
         Response<CellDataList> resp = lsAddressClient.createCell(standId, cellDataList);
         lsAddressClient.assertThatCellIsCreated(resp, standId, cellDataList);
-
     }
 
     @Test(description = "C3316323 lsAddress GET cells")
     public void testGetCells() {
-        step("Create a new alley");
-        AlleyData alleyData = lsAddressHelper.createRandomAlley();
-        createdAlleyId = alleyData.getId();
-
-        step("Create new stands");
-        standDataList = lsAddressHelper.createDefaultStands(alleyData);
-
+        prepareDefaultData(false, true);
         step("Get first stand from list");
         StandData standData = lsAddressHelper.getStandFromList(0, alleyData.getId());
-
-        step("Create new cells");
-        cellDataList = lsAddressHelper.createDefaultCells(standData.getId());
 
         step("Get cells from list");
         int standId = standDataList.getItems().get(0).getId();
@@ -192,18 +193,7 @@ public class LsAddressTest extends BaseProjectApiTest {
 
     @Test(description = "C23194975 lsAddress PUT cells - Add item")
     public void testUpdateCells() {
-        step("Create a new alley");
-        AlleyData alleyData = lsAddressHelper.createRandomAlley();
-        createdAlleyId = alleyData.getId();
-
-        step("Create new stands");
-        standDataList = lsAddressHelper.createDefaultStands(alleyData);
-
-        step("Get first stand from list");
-        StandData standData = lsAddressHelper.getStandFromList(0, alleyData.getId());
-
-        step("Create new cells");
-        cellDataList = lsAddressHelper.createDefaultCells(standData.getId());
+        prepareDefaultData(true, true);
 
         step("Prepare cells data to update");
         cellData.setType(5);
@@ -227,18 +217,7 @@ public class LsAddressTest extends BaseProjectApiTest {
 
     @Test(description = "C23194977 lsAddress DELETE cells")
     public void testDeleteCell() {
-        step("Create a new alley");
-        AlleyData alleyData = lsAddressHelper.createRandomAlley();
-        createdAlleyId = alleyData.getId();
-
-        step("Create new stands");
-        standDataList = lsAddressHelper.createDefaultStands(alleyData);
-
-        step("Get first stand from list");
-        StandData standData = lsAddressHelper.getStandFromList(0, alleyData.getId());
-
-        step("Create new cells");
-        cellDataList = lsAddressHelper.createDefaultCells(standData.getId());
+        prepareDefaultData(true, true);
 
         step("Delete cell");
         int itemIndexToRemove = 0;
@@ -255,7 +234,20 @@ public class LsAddressTest extends BaseProjectApiTest {
     // Cell products
     @Test(description = "C23194989 lsAddress POST Cell products")
     public void testCreateCellProducts() {
-        // Test data
+        step("Create a new alley");
+        AlleyData alleyData = lsAddressHelper.createRandomAlley();
+        createdAlleyId = alleyData.getId();
+
+        step("Create new stands");
+        standDataList = lsAddressHelper.createDefaultStands(alleyData);
+
+        step("Get first stand from list");
+        StandData standData = lsAddressHelper.getStandFromList(0, alleyData.getId());
+
+        step("Create new cells");
+        cellDataList = lsAddressHelper.createDefaultCells(standData.getId());
+
+        step("Prepare test data");
         CellData cellData = cellDataList.getItems().get(0);
         String cellId = cellData.getId();
         String lmCode = searchProductHelper.getProducts(1).get(0).getLmCode();
